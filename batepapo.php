2bb755ab81d3757afae8b8a76dbe26a11980fb17
MateusAgostinho1 <95450956+MateusAgostinho1@users.noreply.github.com>
2022-02-02 17:37:51 +0000

    Add files via upload

---
+++ batepapo.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.0.2
+ */
+
+/**
+ * Database `batepapo`
+ */
+
+/* `batepapo`.`mensagens` */
+$mensagens = array(
+  array('id' => '8','Nome' => 'Joelson AntÃ³nio','msg' => 'Cassule vem agora','data_msg' => '2022-01-25 21:42:45')
+);
